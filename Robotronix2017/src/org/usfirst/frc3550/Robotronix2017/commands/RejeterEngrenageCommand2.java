f8bc327b8c4cb59de31a5b714efa060b1be32cf6
PROG-8 <PROG-8@10.74.11.43>
2017-01-21 13:15:21 -0500

    test

---
+++ Robotronix2017/src/org/usfirst/frc3550/Robotronix2017/commands/RejeterEngrenageCommand2.java
@@ -0,0 +1,40 @@
+package org.usfirst.frc3550.Robotronix2017.commands;
+
+import org.usfirst.frc3550.Robotronix2017.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class RejeterEngrenageCommand2 extends Command {
+
+    public RejeterEngrenageCommand2() {
+        // Use requires() here to declare subsystem dependencies
+        requires(Robot.ramasseur);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.ramasseur.Eject();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.ramasseur.Stop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
