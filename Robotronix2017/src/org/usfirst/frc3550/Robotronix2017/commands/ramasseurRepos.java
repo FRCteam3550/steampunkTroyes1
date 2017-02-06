1b8eec7d571b303d299608eceeb6dd6c44ff0a0d
PROG-10 <PROG-10@10.74.11.149>
2017-02-06 15:54:35 -0500

    modification position de depart des pistons

--- Robotronix2017/src/org/usfirst/frc3550/Robotronix2017/commands/ramasseurRepos.java
+++ Robotronix2017/src/org/usfirst/frc3550/Robotronix2017/commands/ramasseurRepos.java
@@ -22,7 +22,7 @@ public class ramasseurRepos extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	Robot.ramasseur.Stop();
-    	Robot.ramasseur.Extract();
+    	Robot.ramasseur.Retract();
     	Robot.ramasseur.Down();
     }
 
